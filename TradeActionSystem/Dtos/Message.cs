4296ee75c8f1112c5bd865d8e883b0d7c72605b0
luke-d-moore <luke.d.moore1@gmail.com>
2025-11-12 12:23:38 +0000

    change to message class to get uniqueid
    change to message class to get uniqueid

--- TradeActionSystem/Dtos/Message.cs
+++ TradeActionSystem/Dtos/Message.cs
@@ -6,15 +6,11 @@ namespace TradeActionSystem.Dtos
     {
         [JsonPropertyName("Ticker")]
         public string Ticker { get; set; }
+        [JsonPropertyName("UniqueID")]
+        public string UniqueID { get; set; }
         [JsonPropertyName("Quantity")]
         public int Quantity { get; set; }
         [JsonPropertyName("Action")]
         public string Action { get; set; }
-        public Message(string ticker, int quantity, string action)
-        {
-            Ticker = ticker;
-            Quantity = quantity;
-            Action = action;
-        }
     }
 }
